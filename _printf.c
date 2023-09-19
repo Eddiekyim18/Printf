09772dfc6858afc473596b79e1aac9033afb0d45
eddiekyim18 <osakueeddiekyim@gmail.com>
2023-09-19 05:15:47 -0700

    printf.c file ammended

--- _printf.c
+++ _printf.c
@@ -1,74 +1,62 @@
-#include "nain.h"
-
-int print_char(int c)
-{
-return (write(1, &c, 1));
-}
-
-int print_str(const char *str)
-{
-int char_count = 0;
-while (*str)
-{
-char_count += print_char(*str);
-str++;
-}
-return (char_count);
-}
-
+#include "main.h"
 int _printf(const char *format, ...);
-
-int _printf(const char *format, ...)
 {
-va_list args;
-va_start(args, format);
+int character_print = 0;
+va_list eddie_arg;
 
-int char_count = 0;
+if (format == NULL)
+return (-1);
+
+va_start(eddie_arg, format);
+
 while (*format)
 {
-if (*format == '%')
+if (*format != '%')
 {
-format++; '%'
-switch (*format)
-{
-case 'c':
-{
-int c = va_arg(args, int);
-char_count += print_char(c);
-break;
-}
-case 's':
-{
-const char *str = va_arg(args, const char *);
-char_count += print_str(str);
-break;
-}
-case '%':
-{
-char_count += print_char('%');
-break;
-}
-default:
-{
-break;
-}
-}
+write(1, format, 1);
+character_print++;
 }
 else
 {
-char_count += print_char(*format);
+format++;
+if (*format == '\0')
+break;
+
+if (*format == '%')
+{
+write(1, format, 1);
+character_print++;
+}
+
+else if (format == 'c')
+{
+char c = va_arg(eddie_arg, int);
+write(1, &c, 1);
+character_print++;
+}
+
+else if (format == 's')
+{
+char *str va_arg(eddie_arg, char*);
+int str_len = 0
+while (str(str_len) != '0')
+str_len++;
+write(1, str, str_len);
+character_print += str_len;
 }
 format++;
 }
-
-va_end(args);
-return (char_count);
+va_end(eddie_arg);
+}
+return (character_print++);
 }
 
 int main(void)
-}
-int count = _printf("Hello, %s! The answer is %d%%\n", "world", 42);
-printf("Total characters printed: %d\n", count);
+{
+_printf("Leo\n");
+_printf("%c\n", 'v');
+_printf("%s\n", "string");
+_printf("%%/n");
+
 return (0);
 }
-
