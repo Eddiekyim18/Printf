4889edb299af351ca51ecba02d6fdd28f972b844
3hree6ix9ine <braithee@gmail.com>
2023-09-20 11:29:41 -0700

    Wesley/Osakwe

--- get_precision.c
+++ get_precision.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * get_precision - Calculates the precision
- * @format: Formatted string to print arguments
- * @i: Pointer to current position in fmt string
+ * get_precision - Knowing the precision
+ * @format: string argument
+ * @i: current fmt string
  * @list: Argument list
  * Return: eachtime precision
  */
+
 
 int get_precision(const char *format, int *i, va_list list)
 {
@@ -14,29 +15,31 @@ int get_precision(const char *format, int *i, va_list list)
 	int precision = -1;
 
 	if (format[curr_i] != '.')
-		return (precision);
+	{
+		return precision;
+	}
 
 	precision = 0;
+	curr_i++;
 
-	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+	while (format[curr_i] != '\0')
 	{
-		if (is_digit(format[curr_i]))
+		if (isdigit(format[curr_i]))
 		{
 			precision *= 10;
 			precision += format[curr_i] - '0';
-		}
-		else if (format[curr_i] == '*')
+			curr_i++;
+		} else if (format[curr_i] == '*')
 		{
 			curr_i++;
 			precision = va_arg(list, int);
 			break;
-		}
-		else
+		} else {
 			break;
+		}
+
+		*i = curr_i - 1;
+
+		return (precision);
 	}
-
-	*i = curr_i - 1;
-
-	return (precision);
-}
 
