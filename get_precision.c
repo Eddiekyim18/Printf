34a301602cbd475556004bf77a3b3fce1ef0cbea
3hree6ix9ine <braithee@gmail.com>
2023-09-20 11:36:15 -0700

    Wesley/Osakwe

--- get_precision.c
+++ get_precision.c
@@ -8,36 +8,35 @@
  * Return: eachtime precision
  */
 
-
 int get_precision(const char *format, int *i, va_list list)
 {
 	int curr_i = *i + 1;
 	int precision = -1;
 
 	if (format[curr_i] != '.')
-	{
 		return (precision);
-	}
 
 	precision = 0;
-	curr_i++;
 
-	while (format[curr_i] != '\0')
+	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
 	{
-		if (isdigit(format[curr_i]))
+		if (is_digit(format[curr_i]))
 		{
 			precision *= 10;
 			precision += format[curr_i] - '0';
-			curr_i++;
-		} else if (format[curr_i] == '*')
+		}
+		else if (format[curr_i] == '*')
 		{
 			curr_i++;
 			precision = va_arg(list, int);
 			break;
 		}
-
-		*i = curr_i - 1;
-
-		return (precision);
+		else
+			break;
 	}
 
+	*i = curr_i - 1;
+
+	return (precision);
+
+}
